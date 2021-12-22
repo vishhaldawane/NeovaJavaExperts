486c8157324fd3e2d9c15442b6923cdf0cd476d3
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2021-12-22 20:03:57 +0530

    Add files via upload

---
+++ Greeting.java
@@ -0,0 +1,71 @@
+package com.neova;
+
+public class Greeting {
+
+	public static void main(String abc[]) {
+		
+		System.out.println("Welcome Again to Java");
+		System.out.println("1. Java is simple");
+		System.out.println("2. Java is WORA");
+		System.out.println("3. Java is Secured");
+		System.out.println("4. Java is Robust");
+		System.out.println("5. Java is Dynamic");
+		moon();
+		sun();
+		
+		MyMusic.listeningMusic();
+	}
+	public static void moon() {
+		System.out.println("Look at the moon");
+		sun(); //wiring
+	}
+	public static void sun() {
+		System.out.println("Feel the warmth of the Sun");
+	}
+	
+}
+
+class MyMusic
+{
+	public static void main(String abc[]) {
+		System.out.println("MyMusic main...");
+	}
+	
+	public static void listeningMusic()
+	{
+		System.out.println("Listening the music...");
+	}
+	
+}
+/*
+  
+  
+  															first program	
+  															22-Dec-2021
+1 Abhijeet        4       5       testing
+2 Abhishek        4       4									inactive
+3 Akshay G        6       1
+4 Akshay K        2       3.5     manual testing
+5 Akshay          4       2       js
+
+6 Anuj            1       2       manual testing
+7 Bhushan         5/6     14      C#/java/f/w code
+8 Darshana        3       6       automated/manual testing
+9 Isha                    8       cloud/advertising
+10 Jaya            11      5       testing
+
+11 Manasi          3       5       testing
+12 Minal           2       5.5
+13 Pramod
+14 Prashant        1       5       testing
+15 Ravi            2       2       testing
+
+16 Ruhi            5       4.5     testing
+17 Rushikesh
+18 Shubham         6.5     1       testing
+19 Sukant          4       1+      javascript
+20 Swapnil         2.5     9+      teamlead
+21 Vinay      
+
+
+*/
---
+++ Intro.java
@@ -0,0 +1,176 @@
+
+public class Intro {
+
+}
+
+/*
+
+VISHAL DAWANE
+	C C++ JAVA FULL STACK, SPRING HIBERNATE REACT ANGULAR   DEVOPS JENKINS
+	UNIX SHELL SCRIPTING 217TH 
+	ORACLE PL SQL
+	
+	54 MNCS 10000+
+	
+
+		java    years
+Abhijeet 	4	5	testing
+Abhishek	4	4
+Akshay G	6	1
+Akshay K	2	3.5	manual testing
+Akshay		4	2	js
+Anuj		1	2	manual testing
+Bhushan		5/6	14	C#/java/f/w code
+Darshana	3	6	automated/manual testing
+Isha			8	cloud/advertising
+Jaya		11	5	testing
+Manasi		3	5	testing	
+Minal		2	5.5	
+Pramod		1	3	testing
+Prashant	1	5	testing	
+Ravi		2	2	testing
+Ruhi		5	4.5	testing
+Rushikesh	
+Shubham		6.5	1	testing	
+Sukant		4	1+	javascript
+Swapnil		2.5	9+	teamlead	
+Vinay		2	8	testing		
+
+
+C++  (modified version )
+AGENDA DAY1
+-------------
+Introduction to Java
+Your First Java Code
+Java Features
+OOPS concepts
+	1. Abstraction
+	2. Encapsulation
+	3. Poly morphism
+	4. Inheritance
+	5. Object communication
+	
+						who
+						| ? tester, developer, deployer, dba
+				-----------------
+				|			|
+				where		when
+				| space		|time
+			------------------------
+			|	 	  |			|
+			why		what		how
+			reason	inFORMation	 implementation
+									|
+								---------
+								|		|
+								success	feedback
+								
+
+						JAVA TECHNOLOGY
+								|
+					----------------------------
+					|			|			|
+					JSE			JEE			JME
+					standard  enterprise	micro - android
+					core java	|
+					|			|
+		language fundas			------------
+		oops					|		|
+		packages				WEB		Enterprise
+		exception handling		|			|
+		multithreading		  servlets		|
+		generics			  jsp		----------------------------
+		collections						|		|				|
+		file i/o					Session 	Entity			Message
+		jdbc		airindia.com	 | SPRING	|jdbc/Hibernate	|
+								----------	  ----------		------
+								|		|	  |		|			|	|
+						stateless	stateful  BMP	CMP			sms mail
+						
+						Bean managed persistence -your records are stored on the db using jdbc queries
+						Container managed persistence - hibernate will store the records on the db
+								
+	
+	18 days
+	mon - fri = 2 hours
+	sat = 11 to 5 6 hours
+					
+					
+	why java in market from 25 years?
+	
+	
+	1. Secured
+	2. Platform neutral
+	3. Robust - ability to survive in critical situation - 
+					exception handling, 
+					automatic garbage collection
+	4. Object oriented
+	5. Easy to learn / simple
+	6. WORA - write once run anywhere
+	7. Multi-threaded
+	8. Dynamic
+	9. Distributed 
+					
+			Java ->		One Language   - Multiple hardware
+			Dotnet->	Multilanguages - One platform		
+					
+					
+			
+CPU			X86					SPARC			BB			HP		PowerPC/M1			ANY
+OS			windows				Solaris			AIX			hp-ux	Macintosh			Linux
+Compiler	Visual Studio		cc				cc			cc			cc				gcc/g++
+			|					|				|			|			|				|
+		First.cpp/.c	---->  First.cpp		First.cpp	First.cpp	First.cpp	First.cpp
+			|					|				|			|			|				|
+		First.obj			First.o				First.o	First.o			First.o		First.o
+			|					|				|			|			|				|
+		First.exe			First.out		First.out	First.out		First.out	First.out
+		
+		
+		softwired CPU = JVM
+		
+
+		
+CPU			X86					SPARC			BB			HP		APPLE/PowerPC/M1	ANY <--hardwired
+OS			windows				Solaris			AIX			hp-ux	Macintosh			Linux
+Compiler	JVM					JVM				JVM			JVM			JVM				JVM
+			|					|				|			|			|				|
+		First.java				|				|			|			|				|
+			|					|				|			|			|				|
+		First.class			--->			--->		--->		--->			--->
+		(BYTECODE)
+		
+		TOMORROW U INVENT A NEW DEVICE A NEW OS -> JAVA CODE WOULD RUN ON IT, JVM WOULD BE THERE FOR IT
+			
+		
+		OUR FIRST JAVA
+		
+		
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
