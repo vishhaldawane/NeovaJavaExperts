c874954c2d344adbe28adb2f2c9fde4eaecc5a04
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2021-12-23 19:48:58 +0530

    REVISED VERSION TO SET AGE NAME GENDER

--- AssociationTest.java
+++ AssociationTest.java
@@ -7,12 +7,17 @@ public class AssociationTest {
 		//System.out.println("x is "+x);
 		Person thePerson = new Person();
 		thePerson.showGender();
-		thePerson.setGender('T');
+		thePerson.setGender('M');
 		thePerson.showGender();
 		
 		//thePerson.gender='T'; //value is directly set, bypassed the setGender() 
 		//thePerson.showGender();
-
+		
+		thePerson.setAge(22);
+		thePerson.showAge();
+		
+		thePerson.setName("Bash$%ar");
+		thePerson.showName();
 	}
 
 }
@@ -20,7 +25,7 @@ public class AssociationTest {
 class Person
 {
 	private char gender; //not atall a local variable - ITS A FIELD, DATA MEMBER of the class
-	private int age;
+	private int age; //try to set the age as 1 to 120
 	private String name;
 	
 	void setGender(char theGender) { // setter function 
@@ -33,6 +38,40 @@ class Person
 	
 	void showGender() { // accessor - read only activity
 		System.out.println("Gender is : "+gender);
+	}
+	
+	void setAge(int theAge) { // setter function 
+		System.out.println("setting the age..");
+		if(theAge >=1 && theAge<=120)
+			age = theAge;
+		else
+			throw new RuntimeException("Invalid age.....it should be in the range of 1-120");
+	}
+	
+	void showAge() { // accessor - read only activity
+		System.out.println("Age is : "+age);
+	}
+	
+	void setName(String theName) { // setter function 
+		System.out.println("setting the name..");
+		if(theName.matches("^[a-zA-Z]*$"))
+			name = theName;
+		else
+			throw new RuntimeException("Invalid name.....it should have alphabets");
+	}
+	
+	//   12 3  4  567
+	//	 sa x     ena
+	//   sa k  s  ena
+	//   sa[xk]s*ena
+	
+	//^ means starting with a letter
+	//$ means ending with a letter
+	// * means character preceeding it should be matched zero or more times
+	// [] a particular position of a character  
+	 
+	void showName() { // accessor - read only activity
+		System.out.println("Name is : "+name);
 	}
 	
 }
