a91aa77f63afbf645fbcaaa0f93984cc9acc3204
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2021-12-29 20:03:46 +0530

    Add files via upload

---
+++ OopsConcepts.java
@@ -0,0 +1,133 @@
+
+public class OopsConcepts {
+
+}
+/*
+
+	POPS = Procedure Oriented Programming System
+	procedure <---- data
+	top to bottom approach
+	
+	
+	
+	OOPS = Object Oriented Programming System
+	
+	data <--- procedure
+	
+	Surgeon 
+	|
+	
+	cut - length + depth + width  <-- cutting
+	
+	
+	
+	bottom to top approach
+	
+	
+	Tailor Made Shirt
+	|
+	
+	
+	data + function = togetherness is called as contract of class
+	
+	
+	Customer
+	|
+	Tailor =  requirement/measurements + cloth material
+	|
+	paper + pencil/pen
+	|n1,n2,n3 .......n14 <-- one time initialization
+	|
+	|
+	measuring() { } -> makeCuttingDiagram() + chalk
+	1						|2
+							|
+						cutIntoPieces() { } + Scissor
+							|3
+							|
+						stitchingPieces() { } + Needle + stitchingMachine
+							|4
+							|
+						The Shirt is Ready for ironing() + Iron
+								|5
+								PackIt()
+								|5
+	
+	
+	
+	
+	
+	
+	Object = state + behaviour
+				
+	BankAccount = acno, acname, acbalance + withdraw() deposit(), transfer() showBankAccount()
+	 											constructors + setters / getters + toString()
+	 											
+	 		
+	 			
+	 	OOPS concepts
+	 	
+	 		
+	 			
+	 			System.out.println("hello");
+	 			
+	 			
+	 		1. Abstraction
+	 				hiding the complexity of an object
+	 				
+	 				using these functions to utilize the object
+	 				
+	 				it is the "WHAT IS?" part of an object
+	 				
+	 				What is an ATM Object? - getting the cash - Farmer - 
+	 				
+	 				a. data abstraction - how much cash is there in the ATM?
+	 				b. procedural abstraction - cameraClick(), soundRec(), log(), lock() releaseIt()
+	 				c. class abstraction
+	 				
+	 				Kite kite1 = new Kite();
+	 				kite1.fly();
+	 				kite1.kiteFight();
+	 				
+	 				what is your father ?  - he is a Surgeon
+	 				
+	 				
+	 				USAGE
+	 				
+	 				
+	 		2. Encapsulation
+	 				coding for the complexity of the object
+	 				
+	 				binding of data with concerned function = contract
+	 				
+	 				it is the "HOW IS IT?" part of an object
+	 				
+	 				How the cash is dispensed??? - ATM Engineer
+	 				
+	 				class Kite { ....}
+	 				class Flight {... }
+	 				class Person { ....}
+	 				
+	 				CODE for the USAGE
+	 	
+	 		
+	 		3. Polymorphism
+	 		
+	 		4. Inheritance
+	 			
+	 		5. Object communication	
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
