88bdfa92c13019d0aaad457415865dbe2705b759
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2022-01-14 18:25:21 +0530

    THREAD PRIORITY AND ALIVE

--- MultithreadTest2.java
+++ MultithreadTest2.java
@@ -1,4 +1,5 @@
 import java.awt.FlowLayout;
+import java.io.IOException;
 
 import javax.swing.JFrame;
 import javax.swing.JTextField;
@@ -44,14 +45,45 @@ public class MultithreadTest2 {
 		//rail.run();
 		//flight.run();
 		
+		//1-MIN_PRIORITY
+		//5-NORM_PRIORITY
+		//10 MAX_PRIORITY
+		
 		Thread t1 = new Thread(myCar);
 		Thread t2 = new Thread(rail);
 		Thread t3 = new Thread(flight);
+		System.out.println("t1's priority : "+t1.getPriority());
+		System.out.println("t2's priority : "+t2.getPriority());
+		System.out.println("t3's priority : "+t3.getPriority());
 		
+		t1.setPriority(Thread.MIN_PRIORITY);
+		t2.setPriority(Thread.NORM_PRIORITY);
+		t3.setPriority(Thread.MAX_PRIORITY);
+	
+		System.out.println("t1's priority : "+t1.getPriority());
+		System.out.println("t2's priority : "+t2.getPriority());
+		System.out.println("t3's priority : "+t3.getPriority());
+	
+		
+		System.out.println("is t1 alive ? "+t1.isAlive());
+		System.out.println("is t2 alive ? "+t2.isAlive());
+		System.out.println("is t3 alive ? "+t3.isAlive());
+		
+	/*	System.out.println("Press ENTER to continue...");
+		try {
+			System.in.read();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	*/	
 		t1.start(); // it would invoke myCar's run()
 		t2.start(); // it would invoke rail's run();
 		t3.start(); // it would invoke flight's run();
 		
+		System.out.println("is t1 alive ? "+t1.isAlive());
+		System.out.println("is t2 alive ? "+t2.isAlive());
+		System.out.println("is t3 alive ? "+t3.isAlive());
 		
 		
 	//	flight.start();
@@ -92,7 +124,7 @@ class CarFrame extends JFrame implements Runnable //II.a
 		
 	}
 	public void run() { //II.b
-		for(int i=1;i<=1125000;i++ ) {
+		for(int i=1;i<=11125000;i++ ) {
 			//System.out.println("Car completed..."+i);
 			data.setText("Car completed : "+i);
 		}
@@ -110,10 +142,21 @@ class RailwayFrame extends JFrame implements Runnable //II.a
 		add(data);
 	}
 	public void run() { //II.b
-		for(int i=1;i<=1125000;i++ ) {
+		for(int i=1;i<=11125000;i++ ) {
 			//System.out.println("\tRailway completed..."+i);
 			data.setText("Railway completed : "+i);
 		}
+	}
+}
+class BikeFrame extends JFrame implements Runnable
+{
+	JTextField data = new JTextField(20);
+	
+	BikeFrame() {
+		
+	}
+	public void run() {
+		
 	}
 }
 class FlightFrame extends JFrame implements Runnable //II.a
@@ -128,7 +171,7 @@ class FlightFrame extends JFrame implements Runnable //II.a
 		add(data);
 	}
 	public void run() { //II.b
-		for(int i=1;i<=1125000;i++ ) {
+		for(int i=1;i<=11125000;i++ ) {
 			//System.out.println("\t\tFlight completed..."+i);
 			data.setText("Flight completed : "+i);
 		}
