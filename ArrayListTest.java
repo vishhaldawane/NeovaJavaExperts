e64d63c0febbeb339108e420ebe10fec942e659b
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2022-01-18 18:39:41 +0530

    Add files via upload

--- ArrayListTest.java
+++ ArrayListTest.java
@@ -1,31 +1,44 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.TreeSet;
 
 public class ArrayListTest {
 	public static void main(String[] args) {
 		
 		
-		ArrayList myGlass = new ArrayList();//now it is a sac
+	//	ArrayList myGlass = new ArrayList();//now it is a sac
 		
 	//	ArrayList<Integer> myGlass = new ArrayList<Integer>();//now it is NOT a generic sac
+	//	LinkedList<Integer> myGlass = new LinkedList<Integer>();//now it is NOT a generic sac
+	//	TreeSet<Integer> myGlass = new TreeSet<Integer>();//now it is NOT a generic sac
+		HashSet<Integer> myGlass = new HashSet<Integer>();
+		
+		//equals and hashCode
 		
 		System.out.println("Container : myGlass is ready....");
 		
-		myGlass.add(10.0);
-		myGlass.add(25.5f);
-		myGlass.add("Jack");
-		myGlass.add('A');
+		myGlass.add(10);
+		myGlass.add(8);
+		myGlass.add(50);
+		myGlass.add(78);
+		myGlass.add(78);
+		myGlass.add(66);
+		
 		myGlass.add(35);
+		myGlass.add(35);
+		
 		myGlass.add(27);
 		
 		System.out.println("Content [ ButterMilk ] is added...");
 		
-		Iterator myStraw = myGlass.iterator();
+		Iterator<Integer> myStraw = myGlass.iterator();
 		
 		while(myStraw.hasNext()) {
 			//Integer value = (Integer) myStraw.next();
-			Object value = myStraw.next();
-			System.out.println("value : "+value);
+			Integer iValue = myStraw.next();
+			System.out.println("value : "+iValue);
 		}
 		
 		
