243417850a6ab7ef21c1a9e43e3f35eea7ecad33
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2022-01-29 15:44:12 +0530

    Add files via upload

--- SavingsAccountTest.java
+++ SavingsAccountTest.java
@@ -43,6 +43,9 @@ class Bank
 	static SavingsAccount getObject() {
 		return new SavingsAccount(50000);
 	}
+	static SavingsAccount getObject(int x, String y, double z) {
+		return new SavingsAccount(x,y,z);
+	}
 }
 public class SavingsAccountTest
 {
@@ -61,6 +64,50 @@ public class SavingsAccountTest
 		 
 		Assertions.assertEquals(currentBalance-amountToWithdraw1, updatedBalance);
 			System.out.println("All is OK");
+		
+	}
+	
+	@Test
+	public void depositTest() {
+		SavingsAccount savObj1 = Bank.getObject();;
+		
+		Assertions.assertNotNull(savObj1);
+		
+			double currentBalance = savObj1.getBalance(); //50000
+			double amountToWithdraw1=5000;
+			savObj1.deposit(amountToWithdraw1);
+			double updatedBalance =savObj1.getBalance();
+		 
+		Assertions.assertEquals(currentBalance+amountToWithdraw1, updatedBalance);
+			System.out.println("All is OK");
+		
+	}
+	@Test
+	public void fundTransferTest() {
+		SavingsAccount savObj1 = Bank.getObject(101,"Jack",50000);
+		SavingsAccount savObj2 = Bank.getObject(102,"Jill",60000);
+		
+		Assertions.assertNotNull(savObj1);
+		Assertions.assertNotNull(savObj2);
+
+			double amountToTransfer=5000;
+			
+			double sourceBalance = savObj1.getBalance(); //50000
+			double amountToWithdraw=amountToTransfer;
+			savObj1.withdraw(amountToWithdraw);
+			double updatedSourceBalance =savObj1.getBalance();
+			
+			double targetBalance = savObj2.getBalance(); //60000
+			double amountToDeposit=amountToTransfer;
+			savObj2.deposit(amountToDeposit);
+			double updatedTargetBalance =savObj2.getBalance();
+		 
+		Assertions.assertEquals(sourceBalance-amountToWithdraw, updatedSourceBalance);
+			System.out.println("Source is OK");
+		
+		Assertions.assertEquals(targetBalance+amountToDeposit, updatedTargetBalance);
+		
+			System.out.println("Target is OK");
 		
 	}
 }
@@ -90,8 +137,17 @@ class WithdrawTest
 
 class SavingsAccount
 {
+	int acno;
+	String acholder;
 	double balance;
 	
+
+	public SavingsAccount(int acno, String acholder, double balance) {
+		super();
+		this.acno = acno;
+		this.acholder = acholder;
+		this.balance = balance;
+	}
 
 	public SavingsAccount(double balance) {
 		super();
@@ -103,6 +159,12 @@ class SavingsAccount
 		System.out.println("Withdrawing...."+amountToWithdraw);
 		balance = balance - amountToWithdraw; 
 		balance = balance - 1;
+	}
+	
+	void deposit(double amountToDeposit) {
+		System.out.println("Depositing...."+amountToDeposit);
+		balance = balance + amountToDeposit; 
+	//	balance = balance - 1;
 	}
 	double getBalance() {
 		return balance;
