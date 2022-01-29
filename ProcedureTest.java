98654ca48db27ddd0b2ad022705926d9583ed767
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2022-01-29 12:39:30 +0530

    Add files via upload

--- ProcedureTest.java
+++ ProcedureTest.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Types;
 import java.util.Scanner;
 
 public class ProcedureTest {
@@ -33,6 +34,18 @@ public class ProcedureTest {
 			
 			CallableStatement cst = conn.prepareCall("call findDeptDetails(?,?,?)");
 			
+			cst.setInt(1, existingDeptNo);
+			
+			cst.registerOutParameter(2, Types.VARCHAR);
+			cst.registerOutParameter(3, Types.VARCHAR);
+			
+			cst.execute(); // run the procedure;
+			
+			String deptName = cst.getString(2);
+			String deptLoc  = cst.getString(3);
+			
+			System.out.println("DEPT NAME : "+deptName);
+			System.out.println("DEPT LOC  : "+deptLoc);
 			
 			
 			
@@ -59,6 +72,24 @@ public class ProcedureTest {
   	 		dname:=null;
   	 		loc:=null;
   end;
+  / <- to run the script 
+  
+  --------
+  how to call it at oracle prompt
+  
+  SQL> set serveroutput on
+  SQL> declare
+         x varchar2(20);
+         y varchar2(20);
+       begin
+       		findDeptDetails(&no, x, y);
+       		dbms_output.put_line(x);
+       		dbms_output.put_line(y);
+       end;
+       / <--- to run the script
+         
+        
+       		
  */
 
 
